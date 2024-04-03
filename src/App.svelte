8ba8bdf90805e76b0af57a5d227f3734b7413dc9
Towkir Ahmed <towkir17@gmail.com>
2024-04-04 03:25:15 +0600

    added dynamic background banner

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  
+  import DynamicBackground from 'src/lib/DynamicBackground.svelte';
 </script>
 
 <main>
-  
+  <DynamicBackground />
 </main>
 
 <style lang="scss">
---
+++ src/lib/DynamicBackground.svelte
@@ -0,0 +1,33 @@
+<div class="background-cover light-cover"></div>
+<div class="background-cover dark-cover"></div>
+
+<style lang="scss">
+.background-cover {
+  width: 100%;
+  height: 300px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: fixed;
+  top: 0;
+  left: 0;
+  transition: opacity 0.3s ease-in-out;
+  &.light-cover {
+    background-image: url("src/assets/images/light-background.png");
+  }
+  &.dark-cover {
+    background-image: url("src/assets/images/dark-background.png");
+  }
+}
+
+@media screen and (max-width: 570px) {
+  .background-cover {
+    &.light-cover {
+      background-image: url("src/assets/images/light-background-sm.png");
+    }
+    &.dark-cover {
+      background-image: url("src/assets/images/dark-background-sm.png");
+    }
+  }
+}
+</style>
