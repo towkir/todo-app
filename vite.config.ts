4d398c8293aa1c7c6e0357f9f3ae2c27de04428f
Towkir Ahmed <towkir17@gmail.com>
2024-04-04 01:33:11 +0600

    added initial variables and mixins styles

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,25 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-  resolve: {
-    alias: {
-      src: '/src',
-      lib: '/src/lib'
+export default defineConfig((config) => {
+  return {
+    plugins: [svelte()],
+    resolve: {
+      alias: {
+        src: '/src',
+        lib: '/src/lib'
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @import "./src/scss/_variables.scss";
+            @import "./src/scss/_mixins.scss";
+          `,
+        },
+      },
+      devSourcemap: config.mode === 'development',
     },
   }
 })
