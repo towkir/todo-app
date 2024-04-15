186613dce827023fec21270f16131d493ffeaff9
Towkir Ahmed <towkir17@gmail.com>
2024-04-16 00:16:01 +0600

    added initial todo list component

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,14 @@
   import DynamicBackground from 'src/lib/DynamicBackground.svelte';
   import Header from 'src/lib/Header.svelte';
   import TodoInput from 'src/lib/TodoInput.svelte';
+  import TodoList from 'src/lib/TodoList.svelte';
 </script>
 
 <main>
   <DynamicBackground />
   <Header />
   <TodoInput />
+  <TodoList />
 </main>
 
 <style lang="scss">
---
+++ src/lib/TodoList.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import TodoItem from 'src/lib/TodoItem.svelte';
+  
+  let todoItems: TodoType[] = [
+    { title: 'Complete online JavaScript course', isCompleted: true },
+    { title: 'Jog around the park 3x', isCompleted: false },
+    { title: '10 minutes meditation', isCompleted: false },
+    { title: 'Read for 1 hour', isCompleted: false },
+    { title: 'Pick up groceries', isCompleted: false },
+    { title: 'Complete Todo App on Frontend Mentor', isCompleted: false }
+  ];
+</script>
+
+<div class="todo-list">
+  <div class="todo-list-scroller">
+    {#each todoItems as todo}
+      <TodoItem todoItem={todo}/>
+    {/each}
+  </div>
+  <div class="status-bar"></div>
+</div>
+
+<style lang="scss">
+  .todo-list {
+    background-color: $card-bg-color;
+    border-radius: 5px;
+    overflow: hidden;
+    box-shadow: 0 35px 50px -15px $shadow-color;
+    @include transition($properties: box-shadow);
+  }
+</style>
